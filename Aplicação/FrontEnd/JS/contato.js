da352eda6465ed4bd6d477c69a401eeaa5a5d222
Lucas Maia <lmaiarocha23@gmail.com>
2025-04-02 15:30:13 -0300

    Att teste

--- Aplicação/FrontEnd/JS/contato.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("form-suporte").addEventListener("submit", function (e) {
-    e.preventDefault();
-    alert("Sua mensagem foi enviada com sucesso! Em breve entraremos em contato.");
-    this.reset();
-  });
